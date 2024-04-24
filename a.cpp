dc0a17dc1d6a05713d46afbad10186b847f14c3f
F <63239460+F-WTB@users.noreply.github.com>
2024-04-24 16:40:23 +0900

    [update]ファイルの用意

---
+++ a.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using bint = boost::multiprecision::cpp_int;
+using mint = modint998244353;
+using pr = pair<int, int>;
+ostream &operator<<(ostream &os, const mint &x)
+{
+    os << x.val();
+    return os;
+}
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+struct fast
+{
+    fast()
+    {
+        cin.tie(0);
+        ios::sync_with_stdio(0);
+    }
+} fast;
+int main()
+{
+    cout << "Hello world!" << '\n';
+}
